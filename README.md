3059a9cf1b77984746427f5cbdbd15501b071612
lili0415 <123956783+lili0415@users.noreply.github.com>
2023-04-22 19:12:54 +0800

    up BAI

---
+++ README.md
@@ -0,0 +1,69 @@
+# BAI
+## Steps to get started
+To setup the environment, we use `conda` to manage our dependencies.
+
+Our developers use `CUDA 10.1` to do experiments.
+
+You can specify the appropriate `cudatoolkit` version to install on your machine in the `environment.yml` file, and then run the following to create the `conda` environment:
+```bash
+conda env create -f environment.yml
+```
+You shall manually install the following dependencies.
+```bash
+# Install mmcv
+## CAUTION: The latest versions of mmcv 1.5.3, mmdet 2.25.0 are not well supported, due to bugs in mmdet.
+pip install mmcv-full==1.4.3 -f https://download.openmmlab.com/mmcv/dist/cu101/torch1.7.0/index.html
+
+# Install mmdet
+pip install openmim
+mim install mmdet==2.20.0
+
+# Install coco panopticapi
+pip install git+https://github.com/cocodataset/panopticapi.git
+
+# For visualization
+conda install -c conda-forge pycocotools
+pip install detectron2==0.5 -f \
+  https://dl.fbaipublicfiles.com/detectron2/wheels/cu101/torch1.7/index.html
+
+# If you're using wandb for logging
+pip install wandb
+wandb login
+
+# If you develop and run openpsg directly, install it from source:
+pip install -v -e .
+# "-v" means verbose, or more output
+# "-e" means installing a project in editable mode,
+# thus any local modifications made to the code will take effect without reinstallation.
+```
+
+To explore and download their original Dataset:
+https://psgdataset.org/index.html
+Or you can directly download their original dataset using the link they provided: 
+https://entuedu-my.sharepoint.com/:f:/g/personal/jingkang001_e_ntu_edu_sg/EgQzvsYo3t9BpxgMZ6VHaEMBDAb7v0UgI8iIAExQUJq62Q?e=fIY3zh 
+And some pre-trained models that possibly be used:
+https://drive.google.com/drive/folders/1PY0JPCtkOS5Db8-CTFgORvWlOWsYiHfq?usp=sharing
+
+
+Our codebase accesses the datasets from `./data/` and pre-trained models from `./work_dirs/checkpoints/` by default.
+
+Our checkpoint for PSGTR is available at:
+
+https://drive.google.com/file/d/1P2W2PXdRVuv9o_MUSudY7cY0jC3i1xHa/view?usp=sharing
+
+Our new annotation (enhanced by BAI with transfer) for PSG dataset is available at:
+
+https://drive.google.com/file/d/16U5Cp20Z9K5FN2FR8w7qnT_YtVsHJb4J/view?usp=sharing
+
+Users should change the json filename in config/_base_/datasets/psg.py (Line 4-5) to './data/psg/psg.json'.
+
+## Testing
+Run the scripts below:
+```bash
+PYTHONPATH='.':$PYTHONPATH  \
+python tools/test.py  \
+configs/psgtr/psgtr_r50_psg.py \
+work_dirs/PATH_TO_PSGTR_CHECKPOINT \
+--eval sgdet
+```
+
---
+++ tools/vis_results.py
@@ -0,0 +1,98 @@
+import argparse
+import os.path as osp
+
+import mmcv
+from mmcv import Config, DictAction
+from mmdet.datasets import build_dataset, replace_ImageToTensor
+
+from openpsg.utils.utils import show_result
+
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='MMDet eval image prediction result for each')
+    parser.add_argument('config', help='test config file path')
+    parser.add_argument('prediction_path',
+                        help='prediction path where test pkl result')
+    parser.add_argument('show_dir',
+                        help='directory where painted images will be saved')
+    parser.add_argument('--show', action='store_true', help='show results')
+    parser.add_argument('--img_idx',
+                        default=[25, 73],
+                        nargs='+',
+                        type=int,
+                        help='which image to show')
+    parser.add_argument('--wait-time',
+                        type=float,
+                        default=0,
+                        help='the interval of show (s), 0 is block')
+    parser.add_argument('--topk',
+                        default=20,
+                        type=int,
+                        help='saved Number of the highest topk '
+                        'and lowest topk after index sorting')
+    parser.add_argument('--show-score-thr',
+                        type=float,
+                        default=0,
+                        help='score threshold (default: 0.)')
+    parser.add_argument('--one_stage', default=False, action='store_true')
+    parser.add_argument(
+        '--cfg-options',
+        nargs='+',
+        action=DictAction,
+        help='override some settings in the used config, the key-value pair '
+        'in xxx=yyy format will be merged into config file. If the value to '
+        'be overwritten is a list, it should be like key="[a,b]" or key=a,b '
+        'It also allows nested list/tuple values, e.g. key="[(a,b),(c,d)]" '
+        'Note that the quotation marks are necessary and that no white space '
+        'is allowed.')
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+
+    mmcv.check_file_exist(args.prediction_path)
+
+    cfg = Config.fromfile(args.config)
+    if args.cfg_options is not None:
+        cfg.merge_from_dict(args.cfg_options)
+
+    # in case the test dataset is concatenated
+    samples_per_gpu = 1
+    if isinstance(cfg.data.test, dict):
+        cfg.data.test.test_mode = True
+        samples_per_gpu = cfg.data.test.pop('samples_per_gpu', 1)
+        if samples_per_gpu > 1:
+            # Replace 'ImageToTensor' to 'DefaultFormatBundle'
+            cfg.data.test.pipeline = replace_ImageToTensor(
+                cfg.data.test.pipeline)
+    elif isinstance(cfg.data.test, list):
+        for ds_cfg in cfg.data.test:
+            ds_cfg.test_mode = True
+        samples_per_gpu = max(
+            [ds_cfg.pop('samples_per_gpu', 1) for ds_cfg in cfg.data.test])
+        if samples_per_gpu > 1:
+            for ds_cfg in cfg.data.test:
+                ds_cfg.pipeline = replace_ImageToTensor(ds_cfg.pipeline)
+
+    # build the dataloader
+    dataset = build_dataset(cfg.data.test)
+    outputs = mmcv.load(args.prediction_path)
+
+    for idx in args.img_idx:
+        print(idx, flush=True)
+        img = dataset[idx]['img_metas'][0].data['filename']
+        result = outputs[idx]
+        out_filepath = osp.join(args.show_dir, f'{idx}.png')
+        show_result(img,
+                    result,
+                    is_one_stage=args.one_stage,
+                    num_rel=args.topk,
+                    out_file=out_filepath)
+
+
+if __name__ == '__main__':
+    main()
